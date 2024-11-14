d3906f32274b7b3e6f7fbd4e0aa3db3a7c0eda50
Than0s <himanshupatil45h@gmail.com>
2024-11-14 10:03:02 +0530

    search fearture added in free_spot_list

--- app/src/main/java/com/application/parkpilotreg/activity/FreeSpotList.kt
+++ app/src/main/java/com/application/parkpilotreg/activity/FreeSpotList.kt
@@ -3,7 +3,9 @@ package com.application.parkpilotreg.activity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.application.parkpilotreg.FreeSpot
 import com.application.parkpilotreg.R
 import com.application.parkpilotreg.adapter.FreeSpotListAdapter
 import com.application.parkpilotreg.databinding.FreeSpotListBinding
@@ -11,30 +13,49 @@ import com.application.parkpilotreg.viewModel.FreeSpotListViewModel
 
 class FreeSpotList : AppCompatActivity() {
     private lateinit var binding: FreeSpotListBinding
+    private lateinit var viewModel: FreeSpotListViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FreeSpotListBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val viewModel = FreeSpotListViewModel()
+        viewModel = FreeSpotListViewModel()
         binding.freeSpotList.layoutManager = LinearLayoutManager(this)
 
         viewModel.freeSpotList.observe(this) {
-            binding.freeSpotList.adapter = FreeSpotListAdapter(
-                context = this,
-                layout = R.layout.free_spot_list_item,
-                freeSpotList = it,
-                onDeleteClick = { documentId ->
-                    viewModel.removeSpot(documentId,
-                        onSuccess = {
-                            Toast.makeText(this, "removed Successfully", Toast.LENGTH_SHORT).show()
-                            viewModel.getFreeSpotList()
-                        },
-                        onFailure = {
-                            Toast.makeText(this, "Failed to remove", Toast.LENGTH_SHORT).show()
-                        }
-                    )
-                }
-            )
+            loadFreeSpotList(it)
         }
+
+        binding.searchView.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                val filteredList = viewModel.freeSpotList.value?.filter {
+                    it.landMark.contains(newText ?: "")
+                } ?: emptyList()
+                loadFreeSpotList(filteredList)
+                return true
+            }
+        })
+    }
+
+    private fun loadFreeSpotList(list: List<FreeSpot>) {
+        binding.freeSpotList.adapter = FreeSpotListAdapter(
+            context = this,
+            layout = R.layout.free_spot_list_item,
+            freeSpotList = list,
+            onDeleteClick = { documentId ->
+                viewModel.removeSpot(documentId,
+                    onSuccess = {
+                        Toast.makeText(this, "removed Successfully", Toast.LENGTH_SHORT).show()
+                        viewModel.getFreeSpotList()
+                    },
+                    onFailure = {
+                        Toast.makeText(this, "Failed to remove", Toast.LENGTH_SHORT).show()
+                    }
+                )
+            }
+        )
     }
 }
